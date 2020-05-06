c5c28faf0fd879127810f0cf11f156dabe46a8d4
Smokyn <smokyn_cosmin@yahoo.com>
2020-05-06 14:22:13 +0300

    folder structure and initial components structured

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import Layout from './components/Layout/Layout';
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <h1>test</h1>
+      <div>
+        <Layout>
+          <p>test</p>
+        </Layout>
       </div>
     );
   }
