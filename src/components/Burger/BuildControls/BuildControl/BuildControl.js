23682dc0916e9832e161953a78d453bd6c4d5773
Smokyn <smokyn_cosmin@yahoo.com>
2020-05-06 16:26:55 +0300

    controllers to add, remove and totalPrice for burg

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -7,8 +7,16 @@ const BuildControl = (props) => {
     <div className={classes.BuildControl}>
       <div className={classes.Label}>
         {props.label}
-        <button className={classes.Less}>Less</button>
-        <button className={classes.More}>More</button>
+        <button
+          className={classes.Less}
+          onClick={props.removed}
+          disabled={props.disabled}
+        >
+          Less
+        </button>
+        <button className={classes.More} onClick={props.added}>
+          More
+        </button>
       </div>
     </div>
   );
