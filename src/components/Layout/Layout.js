76619dd1e52592b5988e6969ade474a8785e17fb
Smokyn <smokyn_cosmin@yahoo.com>
2020-05-10 19:03:59 +0300

    adding navigation items and sidedrawer comp

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,10 +3,12 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../navigation/Toolbar/Toolbar';
+import SideDrawer from '../navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
   <Aux>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
