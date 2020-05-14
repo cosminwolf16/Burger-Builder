2d859d8eaf16a3df4ba119c5b1af9c8c12e3ba96
Smokyn <smokyn_cosmin@yahoo.com>
2020-05-14 21:42:05 +0300

    updating our reducer with add/remove ingredient

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,10 +1,40 @@
 import * as actionTypes from './actions';
 
+const { ADD_INGREDIENT, REMOVE_INGREDIENT } = actionTypes;
+
 const initialState = {
-  ingredients: null,
+  ingredients: {
+    salad: 0,
+    bacon: 0,
+    cheese: 0,
+    meat: 0,
+  },
   totalPrice: 4,
 };
 
-const reducer = (state = initialState, action) => {};
+const reducer = (state = initialState, action) => {
+  const { type, ingredientName } = action;
+
+  switch (type) {
+    case ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [ingredientName]: state.ingredients[ingredientName] + 1,
+        },
+      };
+    case REMOVE_INGREDIENT:
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [ingredientName]: state.ingredients[ingredientName] - 1,
+        },
+      };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
